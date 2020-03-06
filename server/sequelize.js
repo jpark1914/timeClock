428e128d111c1b3035ecf48697732ac34a5c9c32
Jordan Parker <jordanparker@Jordans-MacBook-Pro.local>
2020-03-06 12:05:26 -0600

    Backend, and Database set up

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const PersonModel = require('./models/person');
 const TimeStampModel = require('./models/timeStamp');
+const _ = require('lodash');
+const faker =require('faker');
+const moment = require('moment');
 
 const sequelize = new Sequelize('timeClock', 'jordan', 'Park7937!',
 {
@@ -21,9 +24,24 @@ const TimeStamp = TimeStampModel(sequelize, Sequelize)
 sequelize.sync({force: true})
 .then(() => {
     console.log(`Database & tables created!`)
+    _.times(10, () =>{
+        return Person.create({
+            fname: faker.name.firstName(),
+            lname: faker.name.lastName()
+        })
+        .then(person => {
+            return person.createTimeStamp({
+                timeIn: moment().format('LT'),
+                timeOut: moment().add(4,'hours').format('LT')
+            })
+        })
+    })
+
 })
 Person.hasMany(TimeStamp);
 TimeStamp.belongsTo(Person);
+
+
 
 module.exports ={
     Person,
